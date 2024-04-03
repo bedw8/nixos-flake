4000fcf58c5441c151f8bc779f1a12cdecbc6c88
Benjamin <bedw@bedw.me>
2024-04-03 18:43:03 -0300

    flake and baks

--- flake.nix
+++ flake.nix
@@ -7,21 +7,37 @@
     home-manager.url = github:nix-community/home-manager;
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nur.url = github:nix-community/NUR;
+    nixvim.url = github:nix-community/nixvim;
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
 
   };
   
-  outputs = inputs @ { self, nixpkgs, home-manager, nur, ... }: 
+  outputs = { self, nixpkgs, home-manager, nur, nixvim, ... }@inputs: 
   let
     system = "x86_64-linux";
     user = "bedw";
+    pkgs = nixpkgs.legacyPackages.${system};
+    hmconf = home-manager.lib.homeManagerConfiguration;
   in 
   {
     nixosConfigurations = (
       import ./hosts {
         inherit (nixpkgs) lib;
-        inherit inputs nixpkgs user system home-manager nur;
+        inherit inputs nixpkgs user system home-manager nur nixvim;
       }
     );
+
+    homeConfigurations = {
+      "bedw@bedwpc" = hmconf {
+        inherit pkgs;
+        extraSpecialArgs = { inherit user; };
+        modules = [
+          nixvim.homeManagerModules.nixvim
+          ./hosts/home.nix
+          ./modules/hidpi/home.nix
+        ];
+      };
+    };
   };
 }
 
